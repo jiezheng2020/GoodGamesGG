413cf499c57ea8a78616f1c0b03c8a651b585da0
Kyle <k.powers005@gmail.com>
2021-04-05 22:38:43 -0400

    seeder data created

--- db/seeders/20210405233808-add--users.js
+++ db/seeders/20210405233808-add--users.js
@@ -4,11 +4,11 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
 
       return queryInterface.bulkInsert('Users', [
-        {firstName: 'John', lastName: 'Doe', userName: 'johnTheDoeMan', email: 'JohnnyDoe@john.com', hashedPassword: 'password'},
-        {firstName: 'Kyle', lastName: 'Powers', userName: 'kpThaSavage', email: 'Kyle@Powers.com', hashedPassword: 'PowPow'},
-        {firstName: 'Brent', lastName: 'Arimoto', userName: 'arimotoChanUwu', email: 'Brent@Arimoto.com', hashedPassword: 'Arimoto'},
-        {firstName: 'Kevin', lastName: 'Zheng', userName: 'scrumMaster', email: 'Kevin@Zheng.com', hashedPassword: 'Zheng'},
-        {firstName: 'James', lastName: 'Lentzsch', userName: 'FlyGuy69', email: 'James@castingcouch.net', hashedPassword: 'Lentzsch'},
+        {firstName: 'John', lastName: 'Doe', userName: 'johnTheDoeMan', email: 'JohnnyDoe@john.com', hashedPassword: 'password', createdAt: new Date(), updatedAt: new Date()},
+        {firstName: 'Kyle', lastName: 'Powers', userName: 'kpThaSavage', email: 'Kyle@Powers.com', hashedPassword: 'PowPow', createdAt: new Date(), updatedAt: new Date()},
+        {firstName: 'Brent', lastName: 'Arimoto', userName: 'arimotoChanUwu', email: 'Brent@Arimoto.com', hashedPassword: 'Arimoto', createdAt: new Date(), updatedAt: new Date()},
+        {firstName: 'Kevin', lastName: 'Zheng', userName: 'scrumMaster', email: 'Kevin@Zheng.com', hashedPassword: 'Zheng', createdAt: new Date(), updatedAt: new Date()},
+        {firstName: 'James', lastName: 'Lentzsch', userName: 'FlyGuy69', email: 'James@castingcouch.ent', hashedPassword: 'Lentzsch', createdAt: new Date(), updatedAt: new Date()},
     ], {});
   },
 
