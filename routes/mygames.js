56ff7431afbef1228fd307608dbf99eb77cf0ded
James Lentzsch <jrlentzsch@gmail.com>
2021-04-06 12:56:16 -0700

    fixed library add issues

--- routes/mygames.js
+++ routes/mygames.js
@@ -1,6 +1,7 @@
 /*************************** REQUIRES ***************************/
 const express = require("express");
-const csrf = require('csurf');
+// const csrf = require('csurf');
+const cookieParser = require("cookie-parser");
 const db = require('../db/models');
 const { csrfProtection, asyncHandler, loginReq } = require("../utils");
 /*************************** ROUTER SETUP ***************************/
@@ -8,15 +9,17 @@ const router = express.Router();
 
 /*************************** MIDDLEWARE ***************************/
 // router.use(loginReq())
+router.use(cookieParser())
 /*************************** ROUTES ***************************/
 // /mygames/ get all mygames
-router.get('/mygames/:userId(\\d+)', csrfProtection,
+router.get('/:userId(\\d+)', csrfProtection,
   asyncHandler(async (req, res) => {
     const games = await db.My_games.find()
-    res.render('mygames', {games, csrfToken: req.csrfToken()})
+    res.json({games})
+    // res.render('mygames', {games, csrfToken: req.csrfToken()})
 }));
 // /mygames/ fetch for shelves
-router.get('/mygames/:userId(\\d+)/libraries', csrfProtection,
+router.get('/:userId(\\d+)/libraries', csrfProtection,
   asyncHandler(async (req, res) => {
     const userId = parseInt(req.params.userId, 10)
     const libraries = await db.Libraries.findall({
@@ -24,12 +27,13 @@ router.get('/mygames/:userId(\\d+)/libraries', csrfProtection,
             userId: userId
         }
     })
-    res.render('libraries', {libraries, csrfToken: csrfToken()})
+    res.json({libraries})
+    // res.render('libraries', {libraries, csrfToken: csrfToken()})
 }));
 // /mygames/ post,delete,put
 
 // add to overall mygames list
-router.post('/mygames/:userId(\\d+)/:gameId(\\d+)/add', csrfProtection,
+router.post('/:userId(\\d+)/:gameId(\\d+)/add', csrfProtection,
   asyncHandler(async (req, res) => {
     const gameId = parseInt(req.params.gameId, 10);
     const userId = parseInt(req.params.userId, 10);
@@ -40,39 +44,44 @@ router.post('/mygames/:userId(\\d+)/:gameId(\\d+)/add', csrfProtection,
     }
     
     const mygame = await db.My_games.create({gameStatus, userId, gameId});
-    res.redirect('/mygames/:userId(\\d+)');
+    res.jason({mygame})
+    // res.redirect('/mygames/:userId(\\d+)');
 }));
 
 // add a library
-router.post('mygames/:userId(\\d+)/libraries/add', csrfProtection,
+router.post('/:userId(\\d+)/libraries/add',
   asyncHandler(async (req, res) => {
-      const userId = parsInt(req.params.userId, 10);
+      console.log("hi")
+      const userId = parseInt(req.params.userId, 10);
       const { name } = req.body
 
-      const library = await Library.create({name, userId});
-      res.redirect('/mygames/:userId(\\d+)/libraries');
+      const library = await db.Library.create({name, userId});
+      res.json({ library })
+    //   res.redirect('/mygames/:userId(\\d+)/libraries');
   }));
 
   // add a game to a library
-router.post('mygames/:userId(\\d+)/libraries/:libraryId(\\+)/gameId(\\+)/add', csrfProtection, 
+router.post('/:userId(\\d+)/libraries/:libraryId(\\+)/gameId(\\+)/add', csrfProtection, 
   asyncHandler(async (req, res) => {
         // const userId = parseInt(req.params.userId, 10);
     const libraryId = parseInt(req.params.libraryId, 10);
     const gameId = parseInt(req.params.gameId, 10);
 
     const libraryGame = await Library_game.create({libraryId, gameId});
-    res.redirect('/mygames/:userId(\\d+)/libraries/:libraryId(\\+)');
+      res.json({ libraryGame })
+    // res.redirect('/mygames/:userId(\\d+)/libraries/:libraryId(\\+)');
 }));
 
 // change played status
-router.put('/mygames/:gameId(\\d+)', csrfProtection, 
+router.put('/:gameId(\\d+)', csrfProtection, 
   asyncHandler(async (req, res) => {
     const gameId = parseInt(req.params.id, 10);
     const mygame = await Mygame.findByPk(gameId)
     const { played } = req.body
 
     const newPlayed = await mygame.update({ played })
-    res.redirect('/mygames/:userId(\\d+)')
+      res.json({ newPlayed })
+    // res.redirect('/mygames/:userId(\\d+)')
 }));
 
 // remove game from mygames
@@ -92,7 +101,7 @@ router.delete('/mygames/:userId(\\d+)/:gameId(\\d+)/delete', csrfProtection,
 }))
 
 // remove game from library
-router.delete('/mygames/:userId(\\d+)/libraries/:libraryId(\\d+)/:gameId(\\d+)/delete', 
+router.delete('/:userId(\\d+)/libraries/:libraryId(\\d+)/:gameId(\\d+)/delete', 
   csrfProtection, asyncHandler(async (req, res) => {
     const libraryId = parseInt(req.params.id, 10);
     const gameId = parseInt(req.params.id, 10)
