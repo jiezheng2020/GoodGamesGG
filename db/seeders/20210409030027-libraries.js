7cd85ab9127fabe1ad4ee341927e212c1b9ba302
brentarimoto <74441357+brentarimoto@users.noreply.github.com>
2021-04-09 16:25:19 -0700

    Update 20210409030027-libraries.js

--- db/seeders/20210409030027-libraries.js
+++ db/seeders/20210409030027-libraries.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-<<<<<<< Updated upstream
     return queryInterface.bulkInsert(
       "Libraries",
       [
@@ -44,26 +43,3 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("Libraries", null, {});
   },
-=======
-
-      return queryInterface.bulkInsert('Libraries', [
-        { name: 'John\'s Suggestions', userId: 6, createdAt: new Date(), updatedAt: new Date() },
-        { name: 'Managment Games', userId: 6, createdAt: new Date(), updatedAt: new Date() },
-        { name: 'Play with SO', userId: 6, createdAt: new Date(), updatedAt: new Date()},
-        { name: 'Sims', userId: 6, createdAt: new Date(), updatedAt: new Date() },
-        { name: 'Party Games', userId: 6, createdAt: new Date(), updatedAt: new Date()}
-      ], {});
-  },
-
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('Libraries', null, {});
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
->>>>>>> Stashed changes
-};
