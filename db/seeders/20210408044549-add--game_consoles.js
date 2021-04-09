2833b5a3db2ef6680c7bd1c51facbd7d98325e68
Kevin Zheng <jiezheng2020@gmail.com>
2021-04-09 14:09:56 -0500

    seeding database done

--- db/seeders/20210408044549-add--game_consoles.js
+++ db/seeders/20210408044549-add--game_consoles.js
@@ -1,127 +1,146 @@
 "use strict";
+const models = require("../models");
+const faker = require("faker");
+const Sequelize = require("sequelize");
+const Op = Sequelize.Op;
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert(
-      "Game_consoles",
-      [
-        {
-          gameId: 1,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 2,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 2,
-          consoleId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 2,
-          consoleId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 3,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 4,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 4,
-          consoleId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 4,
-          consoleId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 5,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 5,
-          consoleId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 5,
-          consoleId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 5,
-          consoleId: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 6,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 6,
-          consoleId: 2,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 6,
-          consoleId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 6,
-          consoleId: 4,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 7,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 8,
-          consoleId: 1,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-        {
-          gameId: 8,
-          consoleId: 3,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        },
-      ],
-      {}
-    );
+  up: async (queryInterface, Sequelize) => {
+    const gameconsoles = [
+      {
+        gameId: 1,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 2,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 2,
+        consoleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 2,
+        consoleId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 3,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 4,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 4,
+        consoleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 4,
+        consoleId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 5,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 5,
+        consoleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 5,
+        consoleId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 5,
+        consoleId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 6,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 6,
+        consoleId: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 6,
+        consoleId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 6,
+        consoleId: 4,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 7,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 8,
+        consoleId: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        gameId: 8,
+        consoleId: 3,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+    ];
+
+    const games = await models.Game.findAll({ where: { id: { [Op.gte]: 9 } } });
+    const consoles = await models.Console.findAll();
+
+    for (let i = 0; i < games.length; i++) {
+      const gameId = games[i].id;
+      let randConsole = Math.floor(Math.random() * 5);
+      if (randConsole === 0) randConsole++;
+
+      let newGame = {
+        gameId: gameId,
+        consoleId: randConsole,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      };
+      gameconsoles.push(newGame);
+    }
+
+    return queryInterface.bulkInsert("Game_consoles", gameconsoles, {});
   },
 
   down: (queryInterface, Sequelize) => {
--- db/seeders/20210409030039-library_games.js
+++ db/seeders/20210409030039-library_games.js
@@ -1,35 +1,112 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    
-      return queryInterface.bulkInsert('Library_games', [
-        { gameId: 24, libraryId: 4, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 27, libraryId: 4, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 31, libraryId: 2, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 25, libraryId: 2, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 30, libraryId: 2, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 26, libraryId: 1, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 33, libraryId: 1, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 3, libraryId: 1, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 26, libraryId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 21, libraryId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 6, libraryId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 9, libraryId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 13, libraryId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 2, libraryId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 12, libraryId: 5, createdAt: new Date(), updatedAt: new Date() },
-        { gameId: 11, libraryId: 5, createdAt: new Date(), updatedAt: new Date() },
-      ], {});
+    return queryInterface.bulkInsert(
+      "Library_games",
+      [
+        {
+          gameId: 24,
+          libraryId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 27,
+          libraryId: 4,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 31,
+          libraryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 25,
+          libraryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 30,
+          libraryId: 2,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 26,
+          libraryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 33,
+          libraryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 3,
+          libraryId: 1,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 26,
+          libraryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 21,
+          libraryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 6,
+          libraryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 9,
+          libraryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 13,
+          libraryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 2,
+          libraryId: 3,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 12,
+          libraryId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          gameId: 11,
+          libraryId: 5,
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+    return queryInterface.bulkDelete("Libary_games", null, {});
+  },
 };
