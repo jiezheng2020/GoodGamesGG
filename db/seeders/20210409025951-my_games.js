718c2dfff514b387cac392cd0a31e8d03e29df95
brentarimoto <74441357+brentarimoto@users.noreply.github.com>
2021-04-09 16:24:45 -0700

    Update 20210409025951-my_games.js

--- db/seeders/20210409025951-my_games.js
+++ db/seeders/20210409025951-my_games.js
@@ -2,7 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-<<<<<<< Updated upstream
     return queryInterface.bulkInsert(
       "My_games",
       [
@@ -119,37 +118,3 @@ module.exports = {
   down: (queryInterface, Sequelize) => {
     return queryInterface.bulkDelete("My_games", null, {});
   },
-=======
-
-      return queryInterface.bulkInsert('My_games', [
-        { played: 1, userId: 6, gameId: 24, createdAt: new Date(), updatedAt: new Date() },
-        { played: 2, userId: 6, gameId: 27, createdAt: new Date(), updatedAt: new Date() },
-        { played: 1, userId: 6, gameId: 2, createdAt: new Date(), updatedAt: new Date() },
-        { played: 1, userId: 6, gameId: 21, createdAt: new Date(), updatedAt: new Date() },
-        { played: 2, userId: 6, gameId: 13, createdAt: new Date(), updatedAt: new Date() },
-        { played: 2, userId: 6, gameId: 12, createdAt: new Date(), updatedAt: new Date() },
-        { played: 0, userId: 6, gameId: 26, createdAt: new Date(), updatedAt: new Date() },
-        { played: 1, userId: 6, gameId: 25, createdAt: new Date(), updatedAt: new Date() },
-        { played: 0, userId: 6, gameId: 31, createdAt: new Date(), updatedAt: new Date() },
-        { played: 0, userId: 6, gameId: 6, createdAt: new Date(), updatedAt: new Date() },
-        { played: 2, userId: 6, gameId: 9, createdAt: new Date(), updatedAt: new Date() },
-        { played: 0, userId: 6, gameId: 33, createdAt: new Date(), updatedAt: new Date() },
-        { played: 0, userId: 6, gameId: 3, createdAt: new Date(), updatedAt: new Date() },
-        { played: 1, userId: 6, gameId: 30, createdAt: new Date(), updatedAt: new Date() },
-        { played: 2, userId: 6, gameId: 11, createdAt: new Date(), updatedAt: new Date() }
-      ], {});
-
-  },
-
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('My_games', null, {});
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
->>>>>>> Stashed changes
-};
